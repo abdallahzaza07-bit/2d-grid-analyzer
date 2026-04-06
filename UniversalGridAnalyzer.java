8a5ce397731e1e484b59b6053ed6d54ce660f04f
abdallahzaza07-bit <abdallahzaza07@gmail.com>
2026-04-06 18:53:54 +0400

    Fix formatting issues and improve subgrid max initialization

--- UniversalGridAnalyzer.java
+++ UniversalGridAnalyzer.java
@@ -153,7 +153,7 @@ public class UniversalGridAnalyzer {
 
     public static void subgrid() {
         int sum = 0;
-        int max = grid[1][1];
+        int max = Integer.MIN_VALUE;
 
         for (int i = 1; i <= 3; i++) {
             for (int j = 1; j <= 3; j++) {
